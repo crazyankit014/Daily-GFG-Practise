ce08ca1e71fc8e83d0ea32f7d77295c944a839aa
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-07-30 10:52:37 +0530

    Updated Solution - GfG to GitHub

--- Easy/Anagram/anagram.cpp
+++ Easy/Anagram/anagram.cpp
@@ -10,9 +10,12 @@ class Solution
     //Function is to check whether two strings are anagram of each other or not.
     bool isAnagram(string a, string b){
         
-      sort(a.begin(),a.end());
-      sort(b.begin(),b.end());
-     return a==b;
+    
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+   return a==b;
+      
     }
 
 };
