83c0be90031116306956fbf15b1822213dcb2136
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2023-10-21 15:58:23 +0530

    Added Solution - GfG to GitHub

---
+++ School/Cube root of a number/cube-root-of-a-number.cpp
@@ -0,0 +1,27 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int cubeRoot(int N) {
+        return cbrt(N);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+
+        cin>>N;
+
+        Solution ob;
+        cout << ob.cubeRoot(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
